16eea349c57ece1ae97aa968373210cab0663b19
devinrbopp <devinrbopp@gmail.com>
2021-12-15 14:37:07 -0700

    tidied formatting

--- src/components/JobDetail.js
+++ src/components/JobDetail.js
@@ -3,16 +3,16 @@ import Tasks from './Task';
 import { useParams } from 'react-router-dom'
 import JobForm from './JobForm';
 
-function JobDetail (props) {
+function JobDetail(props) {
     const [jobTasks, setJobTasks] = useState([])
     const [editForm, setEditForm] = useState(false)
 
-    
+
 
     let id = useParams().id
     console.log('THIS IS THE ID', id)
 
-    let currentJob = props.jobs.filter(job =>{
+    let currentJob = props.jobs.filter(job => {
         return job._id === id
     })
 
@@ -20,47 +20,34 @@ function JobDetail (props) {
         editForm ? setEditForm(false) : setEditForm(true)
     }
 
-    let form
+    let content
 
-    // useEffect(() => {
-        if (editForm) {
-            form = (
-                <JobForm handleCheck={()=> {'func'}} handleChange={()=> {'func'}} job={currentJob[0]} handleSubmit={()=> {'func'}} />
-            )
-        } else {
-            form = (
-                <>
+    if (editForm) {
+        content = (
+            <JobForm handleCheck={() => { 'func' }} handleChange={() => { 'func' }} job={currentJob[0]} handleSubmit={() => { 'func' }} />
+        )
+    } else {
+        content = (
+            <>
                 <div className="job-description-div">
-                {form}
-                <p>{currentJob[0].jobTitle}</p>
-                <p>{currentJob[0].company}</p>
-                <p>{currentJob[0].jobDescription}</p>
-                <p>Applied: {currentJob[0].applied ? 'Yes' : 'No'}</p>
-            </div>
-            <button onClick={editToggle}>Edit Job Details</button>
-            <div className="general-notes"> 
-                <h2>General notes box</h2>
-            </div>
-                </>
-            )
-        }
-    // }, [editForm])
+                    {form}
+                    <p>{currentJob[0].jobTitle}</p>
+                    <p>{currentJob[0].company}</p>
+                    <p>{currentJob[0].jobDescription}</p>
+                    <p>Applied: {currentJob[0].applied ? 'Yes' : 'No'}</p>
+                </div>
+                <button onClick={editToggle}>Edit Job Details</button>
+                <div className="general-notes">
+                    <h2>General notes box</h2>
+                </div>
+            </>
+        )
+    }
 
-    return ( 
+    return (
         <div>
             <h1>This is job detail Page</h1>
-            {form}
-            {/* <div className="job-description-div">
-                {form}
-                <p>{currentJob[0].jobTitle}</p>
-                <p>{currentJob[0].company}</p>
-                <p>{currentJob[0].jobDescription}</p>
-                <p>Applied: {currentJob[0].applied ? 'Yes' : 'No'}</p>
-            </div>
-            <button onClick={editToggle}>Edit Job Details</button>
-            <div className="general-notes"> 
-                <h2>General notes box</h2>
-            </div> */}
+            {content}
             <Tasks jobId={id} tasks={currentJob[0].tasks} user={props.user} getJobs={props.getJobs} />
         </div>
     )
