fed7040d756e29d72462da3a2f96143316e820fa
shuzel99 <shuzellide@gmail.com>
2021-12-15 11:56:57 -0500

    delete jobs working :)

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
 
-
 function JobCard (props) {
 
     const markApplied = () => {
@@ -17,13 +16,22 @@ function JobCard (props) {
         .catch(error => console.log(error))
     }
     
+    const deleteJob = () => {
+        fetch(`http://localhost:8000/jobs/${props.job._id}`, {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/JSON', 'Authorization': 'Bearer ' + props.user.token }
+        })
+        .then(() => props.getJobs())
+        .catch(error => console.log(error))
+    }
 
     //map function to display all the job cards
 
     return (
         <div className="JobCard-container">
             <h1> <Link to={`/job/${props.job._id}`}> {props.job.jobTitle} - {props.job.company}</Link></h1>
-            <p> {props.job.applied ? 'Applied!' : <button onClick={markApplied}>Mark as Applied</button>}</p>
+            <p>{props.job.applied ? 'Applied!' : <button onClick={markApplied}>Mark as Applied</button>}</p>
+            <button onClick={deleteJob}>Delete</button>
         </div>
     )
 }
