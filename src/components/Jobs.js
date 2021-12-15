43a12546d31b34f1486a014b479ac5f80430d83d
devinrbopp <devinrbopp@gmail.com>
2021-12-14 18:47:47 -0700

    job page now displays added jobs

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import JobCard from './JobCard';
 
-function Jobs (props) {
+function Jobs(props) {
 
     const [newJob, setNewJob] = useState({
         jobTitle: "",
@@ -26,74 +26,77 @@ function Jobs (props) {
         reset new job to empty values
     }
     */
-   const handleChange = (e) => {
-       setNewJob({...newJob, [e.target.name]: e.target.value})
-   }
+    const handleChange = (e) => {
+        setNewJob({ ...newJob, [e.target.name]: e.target.value })
+    }
 
-   const handleCheck = (e) =>{
-       setNewJob({...newJob, [e.target.name]: e.target.checked})
-   }
+    const handleCheck = (e) => {
+        setNewJob({ ...newJob, [e.target.name]: e.target.checked })
+    }
 
-   const handleSubmit = (e) =>{
-       e.preventDefault()
-       let preJSONBody = {
-           jobTitle: newJob.jobTitle,
-           company: newJob.company,
-           zipCode: newJob.zipCode,
-           //   leaving tasks off for now
-           applied: Boolean(newJob.applied),
-           jobDescription: newJob.jobDescription,
-           owner: props.user._id
-       }
-       fetch("http://localhost:8000/jobs", {
-           method: "POST",
-           body: JSON.stringify(preJSONBody), 
-           headers: { 'Content-Type': 'application/JSON'}
-       }) 
-       .then(response => response.json())
-       .then(postedJob => {
-           setNewJob({
-            jobTitle: "",
-            company: "",
-            zipCode: "",
-            applied: false,
-            jobDescription: "",
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        let preJSONBody = {
+            jobTitle: newJob.jobTitle,
+            company: newJob.company,
+            zipCode: newJob.zipCode,
+            //   leaving tasks off for now
+            applied: Boolean(newJob.applied),
+            jobDescription: newJob.jobDescription,
             owner: props.user._id
-           })
-           //   props.getJobs()
-       })
-       .catch(error => console.log(error))
-   }
+        }
+        fetch("http://localhost:8000/jobs", {
+            method: "POST",
+            body: JSON.stringify(preJSONBody),
+            headers: { 'Content-Type': 'application/JSON' }
+        })
+            .then(response => response.json())
+            .then(postedJob => {
+                setNewJob({
+                    jobTitle: "",
+                    company: "",
+                    zipCode: "",
+                    applied: false,
+                    jobDescription: "",
+                    owner: props.user._id
+                })
+                props.getJobs()
+            })
+            .catch(error => console.log(error))
+    }
 
-    return(
+    const allJobs = props.jobs.map(job => <JobCard job={job} />)
+    console.log(allJobs)
+
+    return (
         <div className="container-div">
             <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="jobTitle">Job Title: </label>
-                <input type="text" name="jobTitle" id="jobTitle" onChange={handleChange} value={newJob.jobTitle}/>
-            </div>
-            <div>
-                <label htmlFor="company">Company: </label>
-                <input type="text" name="company" id="company" onChange={handleChange} value={newJob.company} />
-            </div>
-            <div>
-                <label htmlFor="zipCode">Zip Code: </label>
-                <input type="text" name="zipCode" id="zipCode" onChange={handleChange}  value={newJob.zipCode}/>
-            </div>
-            <div>
-                <label htmlFor="jobDescription">Job Description: </label>
-                <input type="text" name="jobDescription" id="jobDescription" onChange={handleChange} value={newJob.jobDescription}/>
-            </div>
-            <div>
-                <label htmlFor="applied">Applied </label>
-                <input type="checkbox" name="applied" id="applied" onChange={handleCheck} checked={newJob.applied ? "checked" : ""} />
-            </div>
-            <input type="submit" value="submit"  />
+                <div>
+                    <label htmlFor="jobTitle">Job Title: </label>
+                    <input type="text" name="jobTitle" id="jobTitle" onChange={handleChange} value={newJob.jobTitle} />
+                </div>
+                <div>
+                    <label htmlFor="company">Company: </label>
+                    <input type="text" name="company" id="company" onChange={handleChange} value={newJob.company} />
+                </div>
+                <div>
+                    <label htmlFor="zipCode">Zip Code: </label>
+                    <input type="text" name="zipCode" id="zipCode" onChange={handleChange} value={newJob.zipCode} />
+                </div>
+                <div>
+                    <label htmlFor="jobDescription">Job Description: </label>
+                    <input type="text" name="jobDescription" id="jobDescription" onChange={handleChange} value={newJob.jobDescription} />
+                </div>
+                <div>
+                    <label htmlFor="applied">Applied </label>
+                    <input type="checkbox" name="applied" id="applied" onChange={handleCheck} checked={newJob.applied ? "checked" : ""} />
+                </div>
+                <input type="submit" value="submit" />
 
             </form>
-            
+
             <div className="job-cards">
-                < JobCard/>
+                {allJobs}
             </div>
         </div>
     )
