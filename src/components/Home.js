c25e649a443d2d2356cfacb55a0bf8b939d82f13
devinrbopp <devinrbopp@gmail.com>
2021-12-16 10:20:44 -0700

    tasks stored in state on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Routes, Link } from 'react-router-dom'
 
 
 const Home = (props) => {
 	// const { msgAlert, user } = props
+	const [tasks, setTasks] = useState([])
 	console.log('props in home', props)
 
+	useEffect(() => {
+		if (props.user) {
+			fetch('http://localhost:8000/tasks', {
+				headers: {
+					'Authorization': 'Bearer ' + props.user.token 
+				}
+			})
+				.then(response => {
+					return response.json()
+				})
+				.then(response => {
+					response.sort(function compare(a, b) {
+						let dateA = new Date(a.deadline)
+						let dateB = new Date(b.deadline)
+						return dateA - dateB
+					})
+					setTasks(response)
+				})
+				.catch(error => {console.log(error)})
+		}
+	}, [])
+
+
+	
 	return (
 		<>
 			<h2>Home Page</h2>
